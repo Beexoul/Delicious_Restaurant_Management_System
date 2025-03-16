185fbfe8a7bf8d2079eeccae7994b565da77e498
Beexoul <official.beexoul@gmail.com>
2025-03-16 14:19:09 +0545

    Update

--- user_customer/sendfeedback.py
+++ user_customer/sendfeedback.py
@@ -1,5 +1,26 @@
+# sendfeedback.py
+from datetime import datetime
+
+def save_feedback(feedback):
+    with open('feedbacks.txt', 'a') as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] {feedback}\n")
+    print("Feedback submitted successfully!")
+
 def main():
-    print ("in feedback.py ")
+    print("--------------------------------")
+    print("\n--- Submit Feedback ---")
+    print("--------------------------------")
+    print(" ")
+    
+    while True:
+        feedback = input("Enter your feedback (or type 'exit' to finish): ")
+        if feedback.lower() == 'exit':
+            break
+        save_feedback(feedback)
+    
+    print("Thank you for your feedback! Returning to the menu...")
+    input("Press Enter to continue...")
 
 if __name__ == "__main__":
     main()
